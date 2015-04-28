7d202fbd975738b03f8d6f2c6143f3aef2223d7d
Wayne Grant <waynedgrant@gmail.com>
2015-04-28 18:14:37 -0400

    Add Rainfall unit calc code

--- src/RainfallRate.php
+++ src/RainfallRate.php
@@ -1,1 +1,31 @@
-// TODO
+<?php
+
+# Copyright 2015 Wayne D Grant (www.waynedgrant.com)
+# Licensed under the MIT License
+
+class RainfallRate
+{
+    private $inchesPerMinutePerMinute;
+    private $millimetresPerMinute;
+
+    public function __construct($millimetresPerMinute)
+    {
+        if ($millimetresPerMinute != '-')
+        {
+            $this->inchesPerMinute = number_format(($millimetresPerMinute * (1 / 25.4)), 3, '.', '');
+            $this->millimetresPerMinute = number_format($millimetresPerMinute, 2, '.', '');
+        }
+    }
+
+    public function getInchesPerMinute()
+    {
+        return $this->inchesPerMinute;
+    }
+
+    public function getMillimetresPerMinute()
+    {
+        return $this->millimetresPerMinute;
+    }
+}
+
+?>
---
+++ test/RainfallRateTest.php
@@ -0,0 +1,31 @@
+<?php
+
+# Copyright 2015 Wayne D Grant (www.waynedgrant.com)
+# Licensed under the MIT License
+
+class RainfallRateTest extends PHPUnit_Framework_TestCase
+{
+    public function test_rainfall_rate_values_are_null_when_millimetres_per_minute_is_empty()
+    {
+        $testee = new RainfallRate("-");
+        $this->assertNull($testee->getInchesPerMinute());
+        $this->assertNull($testee->getMillimetresPerMinute());
+    }
+
+    public function test_rainfall_rate_values_are_correct_when_millimetres_per_minute_is_not_empty()
+    {
+        $testee = new RainfallRate("0.00");
+        $this->assertSame("0.000", $testee->getInchesPerMinute());
+        $this->assertSame("0.00", $testee->getMillimetresPerMinute());
+
+        $testee = new RainfallRate("10.01");
+        $this->assertSame("0.394", $testee->getInchesPerMinute());
+        $this->assertSame("10.01", $testee->getMillimetresPerMinute());
+
+        $testee = new RainfallRate("50.55");
+        $this->assertSame("1.990", $testee->getInchesPerMinute());
+        $this->assertSame("50.55", $testee->getMillimetresPerMinute());
+    }
+}
+
+?>
